8be353b30b4a8b43489d2f43617960b6aebc9c75
DJkhalid2 <b00144147@mytudublin.ie>
2023-10-16 13:42:22 +0100

    Assignment 2
    need to implement webgl

---
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float spawnAfterTime = 2f;
+    private float spawnTimer = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnTimer -= Time.deltaTime;
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space)&& spawnTimer <= 0)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            spawnTimer = spawnAfterTime;
+        }
+    }
+}
